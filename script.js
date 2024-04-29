5c493b86776b52b6a1296dae30fbd2c6bf685548
Matheus Delazeri <matheusdelazeris@gmail.com>
2024-04-29 20:10:35 -0300

    - UI: melhorias gerais na interface - Feat: evita bandeiras duplicadas - Feat: adicionado loader ao carregar novas bandeiras

--- script.js
+++ script.js
@@ -2,40 +2,50 @@ var originalFlag = null;
 var erasedFlag = null;
 var answer = "";
 
+let answeredFlags = new Set(JSON.parse(sessionStorage.getItem('answeredFlags')) || []);
+
+function saveAnsweredFlags() {
+    sessionStorage.setItem('answeredFlags', JSON.stringify(Array.from(answeredFlags)));
+}
+
 $(document).ready(function () {
-    $.get("https://flags-api-8ul8.onrender.com/random", async function (data, status) 
-    {
+    $('#loader').show();
+
+    // Fetch flag data
+    fetchFlag().then(async function (data) {
         LoadTips(data);
         SetFirstGameStatus();
-        
-        //acess the url and get the svg data
-        flag_url = data.flag;
-        await $.get(flag_url, async function (flag_data, status) 
-        {
+
+        // Access the URL and get the SVG data
+        const flag_url = data.flag;
+        await $.get(flag_url, async function (flag_data, status) {
             originalFlag = await flag_data.getElementsByTagName("svg")[0];
-            answer = await data.name;
+            answer = data.name;
+
+            // Add the flag to the set of answered flags and save the set
+            answeredFlags.add(answer);
+            saveAnsweredFlags();
         });
 
-        //erase the flag to start the game
+        // Erase the flag colors to start the game
         EraseFlagColors(attempts);
+        $('#loader').hide();
     });
 });
 
-//load all countries names to suggestions
-$(document).ready(function () 
-{
-    $.get("https://flags-api-8ul8.onrender.com/names", function (country_data, status) 
-    {
+// Load all countries' names for suggestions
+$(document).ready(function () {
+    $.get("https://flags-api-8ul8.onrender.com/names", function (country_data, status) {
         suggestions = country_data;
         localStorage.setItem("suggestions", JSON.stringify(suggestions));
-    }); 
+    });
 });
 
 $(function () {
     $("form").submit(function (e) {
         return false;
     });
-})
+});
 
 $(function () {
     $("form").submit(function (e) {
@@ -44,10 +54,27 @@ $(function () {
 
         SubmitGuess(guess);
     });
-})
+});
 
-function LoadTips(jsonData)
-{
+async function fetchFlag() {
+    let flagData;
+    let isDuplicateFlag;
+
+    do {
+        flagData = await $.get("https://flags-api-8ul8.onrender.com/random");
+
+        isDuplicateFlag = answeredFlags.has(flagData.name);
+
+        if (!isDuplicateFlag) {
+            break;
+        }
+    } while (isDuplicateFlag);
+
+    return flagData;
+}
+
+function LoadTips(jsonData) {
+    const tips = [];
     tips[0] = "Continent: " + jsonData.continente;
     tips[1] = "Capital: " + jsonData.capital;
     tips[2] = "Language: " + Object.values(jsonData.lingua);
@@ -57,28 +84,23 @@ function LoadTips(jsonData)
     localStorage.setItem("tips", JSON.stringify(tips));
 }
 
-function SetFirstGameStatus()
-{
-    var attempts = 0;
-    var maxAttempts = 5;
+function SetFirstGameStatus() {
+    const attempts = 0;
+    const maxAttempts = 5;
 
     localStorage.setItem("attempts", attempts);
     localStorage.setItem("maxAttempts", maxAttempts);
 }
 
-function SubmitGuess(guess) 
-{
-    var attempts = localStorage.getItem("attempts");
-    var maxAttempts = localStorage.getItem("maxAttempts");
+function SubmitGuess(guess) {
+    let attempts = localStorage.getItem("attempts");
+    const maxAttempts = localStorage.getItem("maxAttempts");
 
-    if (guess.toLowerCase() == answer.toLowerCase())
-    {
+    if (guess.toLowerCase() === answer.toLowerCase()) {
         alert("Congratulations! You guessed the right country!");
-        ShowRightCountry()
+        ShowRightCountry();
         EraseFlagColors(16);
-    }
-    else
-    {
+    } else {
         attempts++;
         localStorage.setItem("attempts", attempts);
         EraseFlagColors(attempts);
@@ -86,28 +108,26 @@ function SubmitGuess(guess)
         UpdateTips();
     }
 
-    if (attempts >= maxAttempts)
-    {
+    if (attempts >= maxAttempts) {
         alert("You have reached the maximum number of attempts. The right country was: " + answer);
-        EraseFlagColors(16)
+        EraseFlagColors(16);
         ShowRightCountry();
     }
 
     localStorage.setItem("attempts", attempts);
 }
 
-function UpdateAttempts(attempt) 
-{
-    var attempts = localStorage.getItem("attempts");
+function UpdateAttempts(attempt) {
+    const attempts = localStorage.getItem("attempts");
     
-    //update text
-    var attemptsDiv = document.getElementById("attempts");
-    var text_num_attempts = attemptsDiv.getElementsByTagName("h3");
-    text_num_attempts[0].innerHTML = "Attempts: " + attempts + "/ 5";
+    // Update text
+    const attemptsDiv = document.getElementById("attempts");
+    const text_num_attempts = attemptsDiv.getElementsByTagName("h3");
+    text_num_attempts[0].innerHTML = "Attempts: " + attempts + "/5";
 
-    //add new attempt
-    var newAttempt = document.createElement("div");
-    var country = document.createElement("p");
+    // Add new attempt
+    const newAttempt = document.createElement("div");
+    const country = document.createElement("p");
     country.innerHTML = attempt;
     
     newAttempt.className = "attempt";
@@ -115,24 +135,22 @@ function UpdateAttempts(attempt)
     attemptsDiv.appendChild(newAttempt);
 }
 
-function UpdateTips()
-{
-    var attempts = localStorage.getItem("attempts");
+function UpdateTips() {
+    const attempts = localStorage.getItem("attempts");
 
-    if (attempts > 4)
-    {
+    if (attempts > 4) {
         return;
     }
 
-    //update text
-    var tipsDiv = document.getElementById("tips");
-    var text_num_tips = tipsDiv.getElementsByTagName("h3");
-    text_num_tips[0].innerHTML = "Tips: " + attempts + "/ 4";
+    // Update text
+    const tipsDiv = document.getElementById("tips");
+    const text_num_tips = tipsDiv.getElementsByTagName("h3");
+    text_num_tips[0].innerHTML = "Tips: " + attempts + "/4";
 
-    //add new tip
-    var tips = JSON.parse(localStorage.getItem("tips"));
-    var newTip = document.createElement("div");
-    var tip = document.createElement("p");
+    // Add new tip
+    const tips = JSON.parse(localStorage.getItem("tips"));
+    const newTip = document.createElement("div");
+    const tip = document.createElement("p");
     tip.innerHTML = tips[attempts - 1];
     
     newTip.className = "tip";
@@ -140,19 +158,15 @@ function UpdateTips()
     tipsDiv.appendChild(newTip);
 }
 
-function ShowRightCountry()
-{
-    var h1 = document.getElementById("guess");
+function ShowRightCountry() {
+    const h1 = document.getElementById("guess");
     h1.innerHTML = answer;
 
-    var attempts = localStorage.getItem("attempts");
+    const attempts = localStorage.getItem("attempts");
 
-    if(attempts >= 5)
-    {
+    if (attempts >= 5) {
         h1.style.color = "red";
-    }
-    else
-    {
+    } else {
         h1.style.color = "green";
     }
 }
@@ -160,29 +174,29 @@ function ShowRightCountry()
 function EraseFlagColors(num_colors) {
     erasedFlag = originalFlag.cloneNode(true);
 
-    newFlag = EraseColors(new XMLSerializer().serializeToString(erasedFlag), num_colors);
+    const newFlag = EraseColors(new XMLSerializer().serializeToString(erasedFlag), num_colors);
     erasedFlag = new DOMParser().parseFromString(newFlag, "image/svg+xml").getElementsByTagName("svg")[0];
 
-    flagDiv = document.getElementById("flag");
+    const flagDiv = document.getElementById("flag");
     flagDiv.removeChild(flagDiv.firstChild);
     flagDiv.appendChild(erasedFlag);
 }
 
 function EraseColors(rawflag, not_erase_num) {
-    // iterate over all string and in any '#' character, replace the next 6 characters with 'ffffff'
-    var i = 0;
-    var flag = rawflag;
-    var dict = {};
+    // Iterate over the flag and replace color
+    let i = 0;
+    let flag = rawflag;
+    const dict = {};
     while (i < flag.length) {
-        if (flag[i] == '#') {
-            end = i;
-            while (flag[end] != "\"" && end < flag.length) {
+        if (flag[i] === '#') {
+            let end = i;
+            while (flag[end] !== "\"" && end < flag.length) {
                 end++;
             }
-            color = flag.substring(i, end)
+            let color = flag.substring(i, end);
 
-            if (color.length != 7 && color.length != 4) {
-                color = '#a'
+            if (color.length !== 7 && color.length !== 4) {
+                color = '#a';
             }
 
             if (dict[color]) {
@@ -191,9 +205,8 @@ function EraseColors(rawflag, not_erase_num) {
             }
 
             if (Object.keys(dict).length < not_erase_num) {
-                if (color != '#fff' && color != '#FFF' && color != '#ffffff' && color != '#FFFFFF') {
+                if (color !== '#fff' && color !== '#FFF' && color !== '#ffffff' && color !== '#FFFFFF') {
                     dict[color] = true;
-                    console.log(color);
                     i++;
                     continue;
                 }
